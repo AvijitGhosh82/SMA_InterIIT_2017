50a4ec5c8814875a361bf9bb965b9be5752340ea
AvijitGhosh82 <avijitg22@gmail.com>
2017-03-13 20:15:25 +0530

    Removed bse

--- test.py
+++ test.py
@@ -2,12 +2,9 @@ import sys
 sys.path.append('../')
 import pandas
 
-import quandl
-quandl.ApiConfig.api_key = 'KfAoeT8ysydZySAKGaE9'
 
 # data = quandl.get('BSE/BOM533171')
-data = quandl.get('BSE/SENSEX')
+# data = quandl.get('BSE/SENSEX')
 
-data.to_csv('sensex.csv', sep='\t', encoding='utf-8')
 
 
