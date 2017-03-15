4864be9b05e4895218f5da0dd8f5c509105468be
AvijitGhosh82 <avijitg22@gmail.com>
2017-03-15 20:45:50 +0530

    Added visualization

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask
 from flask import Flask, render_template,url_for,request,session,redirect
 from flask_session import Session
 
+import pygal
+import csv
+import urllib
+
 import sys
 sys.path.append('../')
-
-from bseapi import BSEDailyStockQuotes 
 
 
 app = Flask(__name__)
 sess=Session()
+
+NASDAQ=['AAPL', 'CSCO', 'INTC', 'MSFT']
+NYSE=['DIS', 'WMT', 'VZ', 'V', 'UTX', 'UNH', 'TRV', 'PG', 'PFE', 'NKE', 'MRK', 'MMM', 'MCD', 'JPM', 
+'JNJ', 'IBM', 'HD', 'GS', 'GE', 'XOM', 'DD', 'KO', 'CVX', 'CAT', 'BA', 'AXP']
+
+global line_chart
+
+from pygal.style import BlueStyle
+
 
 
 @app.route("/")
@@ -20,9 +31,49 @@ def main():
 def index():
 	return render_template('index.html')
 
+@app.route('/index_post', methods=['POST'])
+def index_post():
+	session['share'] = request.form['share']
+	share = session.get('share', None)
+
+
+	testfile = urllib.URLopener()
+	testfile.retrieve("https://www.google.com/finance/historical?output=csv&q="+share, "data/"+share+".csv")
+
+	global line_chart
+
+
+	line_chart = pygal.Line(style=BlueStyle, disable_xml_declaration=True)
+	line_chart.force_uri_protocol = 'http'
+
+	line_chart.title = share+' Stock'
+
+	with open("data/"+share+".csv", 'r') as my_file:
+	    reader = csv.reader(my_file)
+	    my_list = list(reader)
+
+	header = my_list[0]
+	my_list=my_list[1:]
+	my_list.reverse()
+
+
+	pr = []
+	x = []
+	for p in my_list:
+		pr.append(float(p[4]))
+		x.append(p[0])
+
+	line_chart.x_labels = x
+
+	line_chart.add(share, pr)
+
+	return redirect(url_for('panel'))
+
 @app.route("/panel")
 def panel():
-	return render_template('panel.html')
+	share = session.get('share', None)
+
+	return render_template('panel.html', line_chart=line_chart)
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,47 @@
 import sys
 sys.path.append('../')
-import pandas
+import pygal
+import csv
+import urllib
+
+
+share='AAPL'
+
+
+NASDAQ=['AAPL', 'CSCO', 'INTC', 'MSFT']
+NYSE=['DIS', 'WMT', 'VZ', 'V', 'UTX', 'UNH', 'TRV', 'PG', 'PFE', 'NKE', 'MRK', 'MMM', 'MCD', 'JPM', 
+'JNJ', 'IBM', 'HD', 'GS', 'GE', 'XOM', 'DD', 'KO', 'CVX', 'CAT', 'BA', 'AXP']
+
+testfile = urllib.URLopener()
+testfile.retrieve("https://www.google.com/finance/historical?output=csv&q="+share, share+".csv")
+
+
+line_chart = pygal.Line()
+line_chart.title = share+' Stock'
+
+with open(share+".csv", 'r') as my_file:
+    reader = csv.reader(my_file)
+    my_list = list(reader)
+
+header = my_list[0]
+my_list=my_list[1:]
+my_list.reverse()
+
+
+pr = []
+x = []
+for p in my_list:
+	pr.append(float(p[4]))
+	x.append(p[0])
+
+line_chart.x_labels = x
+
+line_chart.add(share, pr)
+
+line_chart.render_to_file('line_chart_stock.svg')
+
+
+
 
 
 # data = quandl.get('BSE/BOM533171')
