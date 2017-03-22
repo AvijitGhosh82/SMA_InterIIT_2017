102e897d1cb9750132906e246b01dd42fb1d8d92
AvijitGhosh82 <avijitg22@gmail.com>
2017-03-22 23:10:24 +0530

    Live Scraper

---
+++ news.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+
+
+def bloomberg(ticker):
+	import requests
+	url = "https://www.bloomberg.com/quote/"+ticker+":US"
+	r  = requests.get(url)
+	data = r.text
+
+	ts=[]
+	title=[]
+	urls=[]
+
+	soup = BeautifulSoup(data)
+	div= soup.find("div", { "class" : "news__state active" })
+	newsitems = div.findAll('article', {'class' : 'news-story'})
+	for n in newsitems:
+		tst=n.find('time', {"class": "news-story__published-at"}).text
+		if(dfhg)
+			ts.append(tst)
+			title.append(n.find('a', {"class": "news-story__url"}).text)
+			urls.append(n.find('a', {"class": "news-story__url"})['href'])
+			u=n.find('a', {"class": "news-story__url"})['href']
+			try:
+				r2  = requests.get(u)
+				print u
+				data2 = r2.text
+				soup2 = BeautifulSoup(data2)
+				div2= soup2.find("div", { "class" : "body-copy" })
+				print div2.text
+			except:
+				continue
+
+
+
+
+bloomberg('AAPL')
